59387ebf69ce6c1e99de2ac8a6243115c6ae6ef2
Eric Boudin <boudin.eric@gmail.com>
2018-09-29 18:22:39 +0200

    Initialisation

---
+++ models/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+<body>
+    <h1>BIENVENUE SUR MON SITE!!!!!</h1>
+</body>
+</html>
---
+++ models/menu.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/menu.css">
+</head>
+<body>
+    <div id="menu">
+        <ul>
+            <li><a href="google.fr">GOOGEULE</a></li>
+        </ul>
+    </div>
+</body>
+</html>
